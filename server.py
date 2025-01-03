af904b077b693a0235fab4507c75a04e5fe3275e
Octavio Vaca Vargas <Octavio.Vargas@finning.com>
2025-01-03 14:52:11 -0600

    Update server.py
    code review changes

--- server.py
+++ server.py
@@ -1,34 +1,15 @@
 import os
+import main from finning_main
 from flask import Flask
 from cfenv import AppEnv
-from hdbcli import dbapi
 
 app = Flask(__name__)
 env = AppEnv()
 
-hana_service = 'hana'
-hana = env.get_service(label=hana_service)
-
 port = int(os.environ.get('PORT', 3000))
 @app.route('/')
 def hello():
-    if hana is None:
-        return "Can't connect to HANA service '{}' – check service name?".format(hana_service)
-    else:
-        conn = dbapi.connect(address=hana.credentials['host'],
-                             port=int(hana.credentials['port']),
-                             user=hana.credentials['user'],
-                             password=hana.credentials['password'],
-                             encrypt='true',
-                             sslTrustStore=hana.credentials['certificate'])
-
-        cursor = conn.cursor()
-        cursor.execute("select CURRENT_UTCTIMESTAMP from DUMMY")
-        ro = cursor.fetchone()
-        cursor.close()
-        conn.close()
-
-        return "Current time is: " + str(ro["CURRENT_UTCTIMESTAMP"])
+   main()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
