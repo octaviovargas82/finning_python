e032edb8d10ade21405a48bfec20cd0cef7f3ba6
Octavio Vaca Vargas <Octavio.Vargas@finning.com>
2025-01-07 15:20:40 -0600

    Update server.py

--- server.py
+++ server.py
@@ -2,14 +2,19 @@ import os
 from finning_main import main
 from flask import Flask
 from cfenv import AppEnv
-
+ 
 app = Flask(__name__)
 env = AppEnv()
-
+ 
 port = int(os.environ.get('PORT', 3000))
-@app.route('/')
+ 
+@app.route('/hello')
 def hello():
+   return "Hello World!"
+ 
+@app.route('/')
+def root():
    main()
-
+ 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
